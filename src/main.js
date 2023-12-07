421116714f3f06afc74f2d4519e9e30d48bfe482
official-artem <grechuck.artem.ag@gmail.com>
2023-12-07 12:15:30 +0200

    add tailwind

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import './assets/main.css'
+// #region styles import
+
+import '@/styles/main.css';
+import './styles/reset.css';
+// #endregion
 
 import { createApp } from 'vue'
 import App from './App.vue'
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ['./src/**/*.{vue,js}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
